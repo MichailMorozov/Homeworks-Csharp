90d5fcda299c903e16f5e06f92e6581661daec69
micha <micha@DESKTOP-NS8QTO7>
2022-05-09 13:28:11 +0700

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+
+namespace Homework
+{
+    class Task
+    {
+        static void Main(string[] args)
+        {
+            string userInput;
+
+            Dictionary<string, string> words = new Dictionary<string, string>();
+
+            words.Add("Компьютер", "устройство для обработки информации");
+            words.Add("Калькулятор", "устройство для проведения математических рассчетов");
+            words.Add("Секундомер", "устройство для рассчета времени");
+            words.Add("Чайник", "устройство для нагревания воды");
+
+            InputWord(out userInput);
+
+            FindWord(words, userInput);
+        }
+
+        static void InputWord(out string input)
+        {
+            Console.WriteLine("Введите слово:");
+            input = Console.ReadLine();
+        }
+
+        static void FindWord(Dictionary<string, string> dictionary, string input)
+        {
+            if (dictionary.ContainsKey(input))
+            {
+                Console.WriteLine($"{input} - {dictionary[input]}");
+            }
+            else
+            {
+                Console.WriteLine("В словаре нет такого слова!");
+            }
+        }
+    }
+}
