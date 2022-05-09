e4725d91a33ff6e8dee74d90f1b28191e652afc0
Morozof Michael <michaelmorozof@gmail.com>
2022-05-09 14:11:53 +0700

    "Finished"

--- Program.cs
+++ Program.cs
@@ -7,27 +7,77 @@ namespace Homework
         static void Main(string[] args)
         {
             string userInput;
+            bool isWork = true;
 
             Dictionary<string, string> words = new Dictionary<string, string>();
 
-            words.Add("Компьютер", "устройство для обработки информации");
-            words.Add("Калькулятор", "устройство для проведения математических рассчетов");
-            words.Add("Секундомер", "устройство для рассчета времени");
-            words.Add("Чайник", "устройство для нагревания воды");
+            ReadDictionary(words);
 
-            InputWord(out userInput);
+            while (isWork)
+            {
+                ShowMenu();
 
-            FindWord(words, userInput);
+                switch (userInput = Console.ReadLine())
+                {
+                    case "1":
+                        AddNewWord(words);
+                        break;
+                    case "2":
+                        FindWord(words);
+                        break;
+                    case "3":
+                        isWork = false;
+                        break;
+                }
+
+                Console.ReadKey();
+                Console.Clear();
+            }
         }
 
-        static void InputWord(out string input)
+        static void ReadDictionary(Dictionary<string, string> dictionary)
         {
+            dictionary.Add("Компьютер", "устройство для обработки информации");
+            dictionary.Add("Калькулятор", "устройство для проведения математических рассчетов");
+            dictionary.Add("Секундомер", "устройство для рассчета времени");
+            dictionary.Add("Чайник", "устройство для нагревания воды");
+        }
+        
+        static void ShowMenu()
+        {
+            Console.WriteLine("1) Ввести новое слово в словарь.\n" +
+                "2) Узнать значение слова.\n" +
+                "3) Выход.");
+        }
+
+        static void AddNewWord(Dictionary<string, string> dictionary) 
+        {
+            string word;
+            string meaningOfWord;
+
+            Console.WriteLine("Введите слово: ");
+            word = Console.ReadLine();
+
+            if (dictionary.ContainsKey(word))
+            {
+                Console.WriteLine("Такое слово уже есть!");
+            }
+            else
+            {
+                Console.WriteLine("Введите значение этого слова: ");
+                meaningOfWord = Console.ReadLine();
+
+                dictionary[word] = meaningOfWord;
+            }            
+        }
+
+        static void FindWord(Dictionary<string, string> dictionary)
+        {
+            string input;
+
             Console.WriteLine("Введите слово:");
             input = Console.ReadLine();
-        }
 
-        static void FindWord(Dictionary<string, string> dictionary, string input)
-        {
             if (dictionary.ContainsKey(input))
             {
                 Console.WriteLine($"{input} - {dictionary[input]}");
