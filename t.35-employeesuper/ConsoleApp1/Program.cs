037cc658bd89d6e097839dd8150f31d1911fcf0f
MichailMorozov <michaelmorozof@gmail.com>
2022-05-09 21:41:11 +0700

    "finish"

---
+++ t.35-employeesuper/ConsoleApp1/Program.cs
@@ -0,0 +1,62 @@
+﻿// See httpsusing System;
+
+namespace Homework
+{
+    class Task
+    {
+        static void Main(string[] args)
+        {
+            bool isWork = true;
+            bool sucess = false;
+            int value = 0;
+            
+            List<int> numbers = new List<int>();
+
+            while (isWork)
+            {
+                ShowMenu();
+                
+                var userInput = Console.ReadLine();
+
+                sucess = int.TryParse(userInput, out value);
+
+                if (sucess)
+                {
+                    numbers.Add(value);
+                }
+                else if (userInput == "sum")
+                {
+                    ComputeSum(numbers);
+                }
+                else if (userInput == "exit")
+                {
+                    isWork = false;
+                }
+                else
+                {
+                    Console.WriteLine("Введено не число!");
+                }
+
+                Console.ReadKey();
+                Console.Clear();
+            }
+        }
+        static void ShowMenu()
+        {
+            Console.WriteLine("Введите число или команду.\n" +
+                "Доступные команды: \"sum\", \"exit\".");
+        }
+
+        static void ComputeSum(List<int> numbers)
+        {
+            int sum = 0;
+
+            foreach (var number in numbers)
+            {
+                sum += number;
+            }
+
+            Console.WriteLine($"Сумма чисел: {sum}");
+        }
+    }
+}
--- t.35-employeesuper/t.35-employeesuper/Class1.cs
+++ t.35-employeesuper/t.35-employeesuper/Class1.cs
@@ -6,11 +6,45 @@ namespace Homework
     {
         static void Main(string[] args)
         {
-            List<int> numbers = ()
-            (
-           5 , 6, 7, 8) ;
-
+            bool isWork = true;
+            int sum = 0;
+            int value = 0;
             
+            List<int> numbers = new List<int>();
+            
+
+            while (isWork)
+            {
+                Console.WriteLine("Введите число или команду.");
+                var userInput = Console.ReadLine();
+
+                if (userInput == "sum")
+                {
+                    foreach (var number in numbers)
+                    {
+                        sum += number;
+                    }
+                    Console.WriteLine($"Сумма чисел: {sum}");                  
+                }
+                else
+                {                 
+                    if (userInput == "exit")
+                    {
+                        isWork = false;
+                    }
+                    else
+                    {
+                        if (int.TryParse(userInput, out value))
+                        {
+                            numbers.Add(value);
+                        }
+                        else
+                        {
+                            Console.WriteLine("Введено не число");
+                        }
+                    }
+                }
+            }
         }
     }
 }
