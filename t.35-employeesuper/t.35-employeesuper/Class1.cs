2a2491836e823c9ac70dc676c51e3c40be333f9b
MichailMorozov <michaelmorozof@gmail.com>
2022-05-09 18:25:15 +0700

    "35"

---
+++ t.35-employeesuper/t.35-employeesuper/Class1.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace Homework
+{
+    class Task
+    {
+        static void Main(string[] args)
+        {
+            List<int> numbers = ()
+            (
+           5 , 6, 7, 8) ;
+
+            
+        }
+    }
+}
