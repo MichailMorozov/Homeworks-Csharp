66bb0e39096259a273d41aaddf3f5db2cebd11b5
MichailMorozov <michaelmorozof@gmail.com>
2022-05-10 10:54:01 +0700

    Finish

--- t.37 - concatenateTwoArrays/t.37 - concatenateTwoArrays/Program.cs
+++ t.37 - concatenateTwoArrays/t.37 - concatenateTwoArrays/Program.cs
@@ -6,8 +6,8 @@ namespace Homework
     {
         static void Main(string[] args)
         {
-            string[] array1 = { "1", "2", "3"};
-            string[] array2 = { "1", "4", "5"};
+            string[] array1 = { "1", "2", "3", "2"};
+            string[] array2 = { "1", "4", "5", "1"};
 
             List<string> collection = new List<string>();
 
@@ -16,7 +16,7 @@ namespace Homework
 
             ShowElements(collection);
         }
-        
+
         static void AddElements(List<string> list, string[] array)
         {
             foreach (string element in array)
@@ -31,7 +31,7 @@ namespace Homework
                 }
             }
         }
-    
+
         static void ShowElements(List<string> list)
         {
             foreach (var elements in list)
